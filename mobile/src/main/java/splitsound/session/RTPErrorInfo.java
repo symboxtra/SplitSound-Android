6e21ca427e83497918f473e24f9cdc214ecc764e
Jack McKernan <jmcker@outlook.com>
2018-02-25 15:36:00 -0500

    Fix static issues

---
+++ mobile/src/main/java/splitsound/session/RTPErrorInfo.java
@@ -0,0 +1,17 @@
+package splitsound.session;
+
+/**
+ * Created by J. McKernan on 2/25/2018.
+ */
+
+public class RTPErrorInfo
+{
+	public int code;
+	public String description;
+	
+	public RTPErrorInfo(int code, String description)
+	{
+		this.code = code;
+		this.description = description;
+	}
+}
