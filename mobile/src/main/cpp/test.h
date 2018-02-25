de4e08716cec78e32d5a89769a392ff5b192cf46
patelneel55 <patelneel55@gmail.com>
2018-02-25 03:21:53 -0500

    Begin creation of RTP library

---
+++ mobile/src/main/cpp/test.h
@@ -0,0 +1,25 @@
+//
+// Created by Neel on 2/25/2018.
+//
+
+#ifndef SPLITSOUND_ANDROID_TEST_H
+#define SPLITSOUND_ANDROID_TEST_H
+
+#include <iostream>
+class test
+{
+public:
+    test()
+    {
+        printf("Test");
+    }
+
+    void lamo()
+    {
+        printf("In lamo");
+    }
+private:
+
+};
+
+#endif //SPLITSOUND_ANDROID_TEST_H
---
+++ mobile/src/main/java/splitsound/protocol/rtcp/RTCPStructs.java
@@ -0,0 +1,56 @@
+package splitsound.protocol.rtcp;
+
+/**
+ * Created by Neel on 2/25/2018.
+ */
+
+public class RTCPStructs
+{
+    static class RTCPCommonHeader
+    {
+        public byte verson;
+        public byte padding;
+        public byte count;
+
+        public byte packettype;
+        short length;
+
+        public RTCPCommonHeader()
+        {}
+    }
+
+    static class RTCPSenderReport
+    {
+        int ntptime_msw;
+        int ntptime_lsw;
+        int rtptimestamp;
+        int packetcount;
+        int octetcount;
+
+        public RTCPSenderReport()
+        {}
+    }
+
+    static class RTCPReceiverReport
+    {
+        int ssrc;
+        byte fractionlost;
+        byte packetslost[] = new byte[3];
+        byte exthighseqnr;
+        int jitter;
+        int lsr;
+        int dlsr;
+
+        public RTCPReceiverReport()
+        {}
+    }
+
+    static class RTCPSDESHeader
+    {
+        byte sdesid;
+        byte length;
+
+        public RTCPSDESHeader()
+        {}
+    }
+}
