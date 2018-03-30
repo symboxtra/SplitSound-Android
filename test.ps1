50b125f803d2168a20e1686da082a56cbb3da6d3
Jack McKernan <jmcker@outlook.com>
2018-03-30 18:46:04 -0400

    Update API, add error check, and rename

--- test.ps1
+++ test.ps1
@@ -1,4 +1,6 @@
+""
 $var = "PixelTest"
+$image = "system-images;android-27;google_apis;x86"
 
 # Check if emulator was provided as argument, set to default if not
 if("$args" -ne "")
@@ -10,8 +12,22 @@ if("$args" -ne "")
 # create a new test AVD
 if((avdmanager list avd | find "Name: $var") -eq $null)
 {
-	echo "Creating test AVD..."
-	echo no | avdmanager create avd -n $var -k "system-images;android-25;google_apis;x86">$null
+	Write-Host "Creating test AVD..." -Foreground Yellow
+	""
+	echo no | avdmanager create avd -n $var -k $image
+
+	# Check if create failed
+	if ( -not $? )
+    {
+        Write-Host "Failed to create test AVD." -Foreground Red
+		""
+		# Write-Host "Please ensure: '$image' is installed."
+		# ""
+        exit 1
+    }
+
+	Write-Host "Created AVD device: $var" -Foreground Green
+
 }
 
 echo "Using AVD device: $var"
@@ -19,7 +35,7 @@ echo "Using AVD device: $var"
 echo "=== Starting emulator in the background ==="
 
 # Start emulator in background
-Start-Job -ScriptBlock {emulator -avd PixelTest} > $null
+Start-Job -ScriptBlock {emulator -avd $var}
 
 # Wait for emulator before proceeding
 Write-Host "Waiting for emulator to boot..." -ForegroundColor Yellow
