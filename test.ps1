3764f6b66a36794f69d537bfdb5f18236f007b47
Jack McKernan <jmcker@outlook.com>
2018-03-31 22:25:46 -0400

    Update scripts

--- test.ps1
+++ test.ps1
@@ -1,51 +1,70 @@
+#########################################################
+#                                                       #
+#      Run test suite and generate coverage reports     #
+#                                                       #
+#########################################################
+
 ""
-$var = "PixelTest"
+$avd = "PixelTest"
 $image = "system-images;android-27;google_apis;x86"
 
 # Check if emulator was provided as argument, set to default if not
 if("$args" -ne "")
 {
-	$var = "$args"
+    $avd = "$args"
 }
 
 # Check if device exists in avdmanager, else
 # create a new test AVD
-if((avdmanager list avd | find "Name: $var") -eq $null)
+if((avdmanager list avd | findstr /e "Name: $avd") -eq $null)
 {
-	Write-Host "Creating test AVD..." -Foreground Yellow
-	""
-	echo no | avdmanager create avd -n $var -k $image
-
-	# Check if create failed
-	if ( -not $? )
+    Write-Host "Creating test AVD..." -Foreground Yellow
+    ""
+    
+    echo no | avdmanager create avd -n $avd -k "$image" > $null
+    
+    # Check if create failed
+    if ( -not $? )
     {
-        Write-Host "Failed to create test AVD." -Foreground Red
-		""
-		# Write-Host "Please ensure: '$image' is installed."
-		# ""
+        ""
+        Write-Host "Failed to create test AVD with package '$image'." -Foreground Red
+        ""
         exit 1
     }
 
-	Write-Host "Created AVD device: $var" -Foreground Green
-
+    Write-Host "Created AVD device: $avd" -Foreground Yellow
+    ""
 }
 
-echo "Using AVD device: $var"
+Write-Host "Using AVD device: $avd" -Foreground Yellow
 ""
-echo "=== Starting emulator in the background ==="
+Write-Host "=== Starting emulator in the background... ===" -Foreground Yellow
+""
 
 # Start emulator in background
-Start-Job -ScriptBlock {emulator -avd $var}
+Start-Job -ScriptBlock {emulator -avd $args[0]} -ArgumentList $avd > $null
 
 # Wait for emulator before proceeding
 Write-Host "Waiting for emulator to boot..." -ForegroundColor Yellow
+adb wait-for-device
 while((adb shell getprop sys.boot_completed) -ne "1")
 {
-	sleep 5
+    sleep 5
 }
-Write-Host "BOOT SUCCESSFUL" -Foreground Green
+Write-Host "BOOT SUCCESSFUL" -Foreground Yellow
 
 ""
-echo "=== Starting build/test... ==="
+Write-Host "=== Starting build/test... ===" -Foreground Yellow
 
 ./gradlew clean jacocoTestReport
+
+""
+Write-Host "Shutting down emulator..." -Foreground Yellow
+
+# Shut down emulator
+adb -s emulator-5554 emu kill
+adb kill-server
+
+""
+
+
--- test.sh
+++ test.sh
@@ -6,36 +6,56 @@
 #                                                       #
 #########################################################
 
-RED='\033[0;32m'
+RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No color
 echo
 
-# Parse args
-avd=none
+avd="PixelTest" # Default AVD
+image="system-images;android-27;google_apis;x86"
 headless=false
-while getopts ":a:h" opt; do
+
+# Parse args
+while getopts ":a:sh" opt; do
     case $opt in
         a)
-            avd=$OPTARG
+            avd="$OPTARG"
             ;;
-        t)
+        s)
             headless=true
-            echo "${YELLOW}Running in headless mode. Remove -h to run non-headless."
+            echo "${YELLOW}Running in headless mode. Remove -s to run non-headless.${NC}"
             echo
             ;;
+        h)
+            echo "Usage: test.sh [-a=avd device name] [-s] [-h]"
+            echo "-s    Run emulator headless."
+            exit 1
+            ;;
     esac
-done        
+done
 
 # Check that emulator was provided as an argument
-if [ -z $avd ]; then
-    echo "No emulator name provided."
-    echo "Usage: test.sh -a emulator-name [-t]"
-    exit 1
+if [ -z `avdmanager list avd | grep "Name: $avd$"` ]; then
+    echo -e "${YELLOW}Creating test AVD...${NC}"
+    echo
+
+    echo no | avdmanager create avd -n "$avd" -k "$image" > /dev/null
+
+    # Check if create failed
+    if [ $? != 0 ]; then
+        echo
+        echo -e "${RED}Failed to create test AVD with package '$image'.${NC}"
+        echo
+        exit 1
+    fi
+
+	echo -e "${YELLOW}Created AVD device: $avd${NC}"
+	echo
+
 fi
 
 # Check that device exists in avdmanager
-if [ -z `avdmanager list avd | grep "Name: $avd"` ]; then
+if [ -z `avdmanager list avd | grep "Name: $avd$"` ]; then
     echo "$avd was not a valid device in avdmanager."
     echo "Valid devices are: "
     avdmanager list avd | grep "Name:" --color=never
@@ -44,36 +64,40 @@ if [ -z `avdmanager list avd | grep "Name: $avd"` ]; then
 fi
 
 echo -e "${YELLOW}Using AVD device: $avd"
-echo -e "${YELLOW}Starting emulator in background...${NC}"
+echo
+echo -e "${YELLOW}=== Starting emulator in background... ===${NC}"
 echo
 
 # Start emulator
 if $headless; then
-    emulator -avd $avd -no-skin -no-audio -no-window -gpu off -no-accel -no-boot-anim &
+    emulator -avd "$avd" -no-skin -no-audio -no-window -gpu off -no-accel -no-boot-anim &
 else
-    emulator -avd $avd &
+    emulator -avd "$avd" &
 fi
 
 # Wait for emulator
+echo -e "${YELLOW}Waiting for emulator to boot...${NC}"
 adb wait-for-device
-
-while [ `adb shell getprop sys.boot_completed | tr -d '\r'` != "1" ]; 
+while [ `adb shell getprop sys.boot_completed | tr -d '\r'` != "1" ];
 do
     sleep 2
-    echo -e "${YELLOW}Waiting for emulator to boot...${NC}"
 done
-
-# Unlock old devices -- might not be needed
-adb shell input keyevent 82 &
+echo -e "${YELLOW}BOOT SUCCESSFUL${NC}"
 
 echo
-echo -e "${YELLOW}Starting build/test...${NC}"
+echo -e "${YELLOW}=== Starting build/test... ===${NC}"
 echo
 
 # Run test and generate coverage reports
 chmod +x ./gradlew
 ./gradlew clean jacocoTestReport
 
-# Remove default coverage report
-# Only need merged Jacoco report
-rm -r ./mobile/build/reports/coverage
+echo
+echo -e "${YELLOW}Shutting down emulator...${NC}"
+
+# Shut down emulator
+adb -s emulator-5554 emu kill
+adb kill-server
+
+echo
+
