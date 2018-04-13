c244b002a3a78d6f4356b21c4aa669536ee9b698
Jack McKernan <jmcker@outlook.com>
2018-04-13 10:13:02 -0400

    Conditionally source tokens.sh

--- send-coverage.sh
+++ send-coverage.sh
@@ -6,5 +6,8 @@
 #                                                       #
 #########################################################
 
-source ./tokens.sh
+if [ -f ./tokens.sh ]; then
+    source ./tokens.sh
+fi
+
 bash <(curl -s https://codecov.io/bash) -t $CODE_COV_TOKEN
