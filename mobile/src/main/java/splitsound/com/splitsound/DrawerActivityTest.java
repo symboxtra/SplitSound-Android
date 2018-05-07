6d8d9688a2bd532a4aeb02d00893ac10388333e8
polak69 <epituch@purdue.edu>
2018-05-07 17:15:14 -0400

    changed to dark theme, changed icon, and changed to orange Next steps are to make it only one main activity

--- mobile/src/main/java/splitsound/com/splitsound/DrawerActivityTest.java
+++ mobile/src/main/java/splitsound/com/splitsound/DrawerActivityTest.java
@@ -70,9 +70,9 @@ public class DrawerActivityTest extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.available_sessions) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } /*else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -83,7 +83,7 @@ public class DrawerActivityTest extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
+        */
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
