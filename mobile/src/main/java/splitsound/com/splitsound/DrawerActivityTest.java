10d1ce369b27c2e6f99ece5945b0c5843add15f0
Neel Patel <patelneel55@gmail.com>
2018-05-15 17:24:01 -0500

    Create RecyclerView for userlist

--- mobile/src/main/java/splitsound/com/splitsound/DrawerActivityTest.java
+++ mobile/src/main/java/splitsound/com/splitsound/DrawerActivityTest.java
@@ -13,16 +13,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.TranslateAnimation;
 
 import splitsound.com.ui.adapters.RecyclerAdapter;
+import splitsound.com.ui.adapters.UserListAdapter;
 
 
 public class DrawerActivityTest extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView uRecyclerView;
+
+    View myView;
+    boolean isUp = false;
 
     public static String helloWorld()
     {
@@ -51,13 +55,25 @@ public class DrawerActivityTest extends AppCompatActivity
         mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // specify an adapter (see also next example)
-        mAdapter = new RecyclerAdapter();
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(new RecyclerAdapter());
         mRecyclerView.setVisibility(View.GONE);
+
+
+        uRecyclerView = findViewById(R.id.user_list_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        uRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        uRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // specify an adapter (see also next example)
+        uRecyclerView.setAdapter(new UserListAdapter());
+        uRecyclerView.setVisibility(View.GONE);
     }
 
     @Override
@@ -121,5 +137,40 @@ public class DrawerActivityTest extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void clicked(View v)
+    {
+        if (isUp) {
+            slideDown(uRecyclerView);
+        } else {
+            slideUp(uRecyclerView);
+        }
+        isUp = !isUp;
+    }
+
+    // slide the view from below itself to the current position
+    public void slideUp(View view){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                view.getHeight(),  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(5000);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDown(View view){
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight()); // toYDelta
+        animate.setDuration(5000);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
     }
 }
---
+++ mobile/src/main/java/splitsound/com/ui/adapters/UserListAdapter.java
@@ -0,0 +1,69 @@
+package splitsound.com.ui.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import splitsound.com.splitsound.R;
+
+/**
+ * Created by Neel on 5/15/2018.
+ */
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHolder> {
+
+    //TODO have actual data, this is just sample data
+    private UserInfo[] users = {
+            new UserInfo("Me", "Nexus 6P", true),
+            new UserInfo("Emanuel", "OnePlus69", false),
+            new UserInfo("Char*", "Test", false),
+            new UserInfo("Jack", "Some phone", true),
+            new UserInfo("Aneesh", "iPhone", false)
+    };
+
+    //provides the views we need to change
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView muteImage;
+        public TextView userName;
+        public TextView userDevice;
+
+        public ViewHolder(View view){
+            super(view);
+            muteImage = itemView.findViewById(R.id.mute_icon);
+            userName = itemView.findViewById(R.id.user_name);
+            userDevice = itemView.findViewById(R.id.device_name);
+        }
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public UserListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_card, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //sets the lockimage to correspond to if there is a password
+        if(users[position].isMuted()) {
+            holder.muteImage.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        }
+        //sets the server name
+        holder.userName.setText(users[position].getName());
+        //sets the server IP address
+        holder.userDevice.setText("Listening on " + users[position].getDeviceName());
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return users.length;
+    }
+}
