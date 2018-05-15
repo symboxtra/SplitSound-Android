e414357c085b9b7b5013ae9c1940b07d5156c0f2
Neel Patel <patelneel55@gmail.com>
2018-05-15 15:27:19 -0500

    Change direct values to reference values

--- mobile/src/main/java/splitsound/com/splitsound/SettingsActivity.java
+++ mobile/src/main/java/splitsound/com/splitsound/SettingsActivity.java
@@ -10,9 +10,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -22,7 +24,6 @@ public class SettingsActivity extends Activity {
 
     public SharedPreferences shrdPref;
     public SharedPreferences.Editor spEdit;
-    public static String username = "";
     final Context context = this;
 
     @Override
@@ -36,52 +37,12 @@ public class SettingsActivity extends Activity {
         shrdPref = this.getPreferences(Context.MODE_PRIVATE);
         spEdit = shrdPref.edit();
 
-        ListView userSettings = (ListView) findViewById(R.id.userSettings);
-        String[] ary = {"Change username..."};
+        // Get ListView and set values and clickListener
+        ListView usListView = (ListView) findViewById(R.id.userSettings);
+        String[] ary = {"Change username...", "Test"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_item, R.id.usrText, ary);
-        userSettings.setAdapter(adapter);
-
-        userSettings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Get the selected item text from ListView
-                String selectedItem = (String) parent.getItemAtPosition(position);
-
-                LayoutInflater li = LayoutInflater.from(context);
-                View promptsView = li.inflate(R.layout.input_dialog, null);
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        context);
-
-                // set prompts.xml to alertdialog builder
-                alertDialogBuilder.setView(promptsView);
-
-                final EditText userInput = (EditText) promptsView.findViewById(R.id.username);
-
-                // set dialog message
-                alertDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("OK",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        // get user input and set it to result
-                                        // edit text
-                                    }
-                                })
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-
-                // show it
-                alertDialog.show();
-            }
-        });
+        usListView.setAdapter(adapter);
+        setListViewClickListener(usListView);
     }
 
     @Override
@@ -96,4 +57,53 @@ public class SettingsActivity extends Activity {
         }
     }
 
+    private void setListViewClickListener(ListView userSettings) {
+        userSettings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item text from ListView
+                String selectedItem = (String) parent.getItemAtPosition(position);
+
+                if (selectedItem.contains("username")) {
+                    LayoutInflater li = LayoutInflater.from(context);
+                    View promptsView = li.inflate(R.layout.input_dialog, null);
+
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            context);
+
+                    // set prompts.xml to alertdialog builder
+                    alertDialogBuilder.setView(promptsView);
+
+                    final EditText userInput = (EditText) promptsView.findViewById(R.id.username);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+                    // set dialog message
+                    alertDialogBuilder
+                            .setCancelable(false)
+                            .setPositiveButton("OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            // get user input and set it to result
+                                            // edit text
+                                            spEdit.putString(getString(R.string.username), userInput.getText().toString());
+                                            spEdit.apply();
+                                        }
+                                    })
+                            .setNegativeButton("Cancel",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+
+                    // create alert dialog
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+
+                    // show it
+                    alertDialog.show();
+                }
+            }
+        });
+    }
 }
