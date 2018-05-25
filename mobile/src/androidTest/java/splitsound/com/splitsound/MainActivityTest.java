b1cfb4cdb5b8b10f9bc91cd85e7b22fd62720978
Neel Patel <patelneel55@gmail.com>
2018-05-25 18:17:39 -0500

    Basic ui-testing for all activities and components

--- mobile/src/androidTest/java/splitsound/com/splitsound/MainActivityTest.java
+++ mobile/src/androidTest/java/splitsound/com/splitsound/MainActivityTest.java
@@ -2,6 +2,8 @@ package splitsound.com.splitsound;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,10 +11,15 @@ import android.support.test.runner.AndroidJUnit4;
 // Common Espresso imports (Do not remove because auto-import does not work with Espresso)
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.contrib.DrawerActions.*;
 import static android.support.test.espresso.action.ViewActions.click;
 import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.Matchers.allOf;
@@ -38,8 +45,43 @@ public class MainActivityTest {
     public ActivityTestRule<DrawerActivityTest> rule = new ActivityTestRule<>(DrawerActivityTest.class);
 
     @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
+    public void test_quick_maths()
+    {
         assertEquals(2 + 2, 4);
+    }
+
+    @Test
+    public void check_play_button()
+    {
+        // Check if main play button exists
+        onView(withId(R.id.main_play_button)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_settings_activity()
+    {
+        // Perform activity change using navigation drawer
+
+        // Open drawer and Settings activity
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.settings));
+    }
+
+    @Test
+    public void swipe_test_user_list()
+    {
+        // Swipe up to open user list
+        onView(withId(R.id.sliding_layout)).perform(swipeUp());
+        onView(withId(R.id.sliding_layout)).perform(swipeDown());
+    }
+
+    @Test
+    public void check_available_sessions() throws InterruptedException
+    {
+        //Open available sessions tab
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.available_sessions));
+        Thread.sleep(100);
+        onView(isRoot()).perform(pressBack());
     }
 }
