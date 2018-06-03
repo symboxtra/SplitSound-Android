4d5b62c691ed0fc2d8c0d02378daaaf78d25b9ba
polak69 <epituch@purdue.edu>
2018-06-03 16:24:25 -0500

    removed 3 dots

--- mobile/src/main/java/splitsound/com/splitsound/DrawerActivityTest.java
+++ mobile/src/main/java/splitsound/com/splitsound/DrawerActivityTest.java
@@ -109,13 +109,6 @@ public class DrawerActivityTest extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer_activity_test, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
