ce634f42cb1880b4a578b508c2f11a6fd1149c7f
polak69 <epituch@purdue.edu>
2018-07-07 21:37:48 -0500

    fixed intro screen so it exits

--- mobile/src/main/java/splitsound/com/splitsound/IntroActivity.java
+++ mobile/src/main/java/splitsound/com/splitsound/IntroActivity.java
@@ -1,56 +1,65 @@
 package splitsound.com.splitsound;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntro2Fragment;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
-public class IntroActivity extends AppIntro {
+public class IntroActivity extends AppIntro2{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Note here that we DO NOT use setContentView();
-
-        // Add your slide fragments here.
-        // AppIntro will automatically generate the dots indicator and buttons.
-
+        getSupportActionBar().hide();
         // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-        addSlide(AppIntroFragment.newInstance("agfd","dfsagf", R.drawable.download,Color.parseColor("#3F51B5")));
+        // Just set a title, description, background and image. AppIntro will do the rest
+        int backgroundColor = Color.DKGRAY;
+        addSlide(AppIntroFragment.newInstance("Welcome!", "This is how to use the app", R.drawable.download, backgroundColor));
 
-        // OPTIONAL METHODS
-        // Override bar/separator color.
-        setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));
-
-        // Hide Skip/Done button.
-        showSkipButton(false);
-        setProgressButtonEnabled(false);
-
-        // Turn vibration on and set intensity.
-        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+        // Turn vibration on and set intensity
+        // NOTE: you will need to ask VIBRATE permission in Manifest if you haven't already
         setVibrate(true);
         setVibrateIntensity(30);
-    }
 
-    /*public void onSkipPressed(Fragment currentFragment) {
-        super.onSkipPressed(currentFragment);
-        // Do something when users tap on Skip button.
-    }
+        // Animations -- use only one of the below. Using both could cause errors.
+        setFadeAnimation(); // OR
 
+        // Permissions -- takes a permission and slide number
+        askForPermissions(new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.VIBRATE}, 1);
+
+    }
+    /*
     public void onDonePressed(Fragment currentFragment) {
-        super.onDonePressed(currentFragment);
-        // Do something when users tap on Done button.
+        System.out.println("reached");
+        Intent intent = new Intent(this, DrawerActivityTest.class); // Call the AppIntro java class
+        startActivity(intent);
+    }*/
+    @Override
+    public void onNextPressed() {
+        // Do something when users tap on Next button.
     }
 
     @Override
-    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
-        super.onSlideChanged(oldFragment, newFragment);
-        // Do something when the slide changes.
-    }*/
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged() {
+        // Do something when slide is changed
+    }
 }
+
+/*
+HOW TO USE: https://github.com/apl-devs/AppIntro/wiki/How-to-Use#show-the-intro-once
+ */
